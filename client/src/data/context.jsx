1a2b092dd681be1fd25cd5ea709f94789371cf83
Mihaela <strechii.mihaela@gmail.com>
2023-05-22 14:29:35 +0200

    routes

--- client/src/data/context.jsx
+++ client/src/data/context.jsx
@@ -18,7 +18,7 @@ export const DataContextProvider = ({ children }) => {
   const backendURL =
     // eslint-disable-next-line no-undef
     process.env.NODE_ENV === 'production'
-      ? 'https://entscape-backend.onrender.com'
+      ? 'https://entscape-backend-2ng6.onrender.com'
       : 'http://localhost:4000';
 
   const heartButtonNotification = (title, action) =>
